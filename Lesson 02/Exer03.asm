4b410e53ed0b349952ba65214153ea4c2dc30c98
MGuian <manaloguian@gmail.com>
2026-08-28 00:56:23 +0800

    Add Exercise 3

---
+++ Lesson 02/Exer03.asm
@@ -0,0 +1,437 @@
+.model small
+.stack
+.code
+
+start:
+    mov ah, 02h
+
+    ; Print ####################
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+
+    ; New Line
+    mov dl, 0dh
+    int 21h
+    mov dl, 0ah
+    int 21h
+
+    ; Print #                   #
+    mov dl, 23h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 23h
+    int 21h
+
+    ; New Line
+    mov dl, 0dh
+    int 21h
+    mov dl, 0ah
+    int 21h
+
+    ; Print #                   #
+    mov dl, 23h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 23h
+    int 21h
+
+    ; New Line
+    mov dl, 0dh
+    int 21h
+    mov dl, 0ah
+    int 21h
+
+    ; Prints #  Guian Jaundell  #
+    mov dl, 23h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 47h
+    int 21h
+    mov dl, 75h
+    int 21h
+    mov dl, 69h
+    int 21h
+    mov dl, 61h
+    int 21h
+    mov dl, 6eh
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 4ah
+    int 21h
+    mov dl, 61h
+    int 21h
+    mov dl, 75h
+    int 21h
+    mov dl, 6eh
+    int 21h
+    mov dl, 64h
+    int 21h
+    mov dl, 65h
+    int 21h
+    mov dl, 6ch
+    int 21h
+    mov dl, 6ch
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 23h
+    int 21h
+
+    ; New Line
+    mov dl, 0dh
+    int 21h
+    mov dl, 0ah
+    int 21h
+
+    ; Print #                   #
+    mov dl, 23h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 23h
+    int 21h
+
+    ; New Line
+    mov dl, 0dh
+    int 21h
+    mov dl, 0ah
+    int 21h
+
+    ; Prints #      Manalo      #
+    mov dl, 23h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 4dh
+    int 21h
+    mov dl, 61h
+    int 21h
+    mov dl, 6eh
+    int 21h
+    mov dl, 61h
+    int 21h
+    mov dl, 6ch
+    int 21h
+    mov dl, 6fh
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 23h
+    int 21h
+
+    ; New Line
+    mov dl, 0dh
+    int 21h
+    mov dl, 0ah
+    int 21h
+
+    ; Print #                   #
+    mov dl, 23h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 23h
+    int 21h
+
+    ; New Line
+    mov dl, 0dh
+    int 21h
+    mov dl, 0ah
+    int 21h
+
+    ; Print #                   #
+    mov dl, 23h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 20h
+    int 21h
+    mov dl, 23h
+    int 21h
+
+    ; New Line
+    mov dl, 0dh
+    int 21h
+    mov dl, 0ah
+    int 21h
+
+    ; Print ####################
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+    mov dl, 23h
+    int 21h
+
+    mov ah, 4ch
+    int 21h
+
+end start
